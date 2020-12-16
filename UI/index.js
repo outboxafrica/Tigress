41317c96c8559e4251adac49f90074c1ff09f099
Bukenya Faizal <bukenyafaizalislam@gmail.com>
2020-12-16 08:32:14 +0300

    updated home page by Faizal

--- UI/index.js
+++
@@ -1,9 +0,0 @@
-function validate(){
-    var password = document.getElementById("password").Value;
-    var cpassword = document.getElementById("cpassword").Value;
-    if(password != cpassword)
-    {
-        alert("Pass and confirm password does not match");
-        return false;
-    }
-}
